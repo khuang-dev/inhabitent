3097ace8ed8affc000de7610ac649ed78f4023d8
khuang-dev <khuang.dev@gmail.com>
2019-11-18 20:43:29 -0800

    final

--- themes/inhabitent/js/scroll-header.js
+++ themes/inhabitent/js/scroll-header.js
@@ -3,7 +3,6 @@
     document.addEventListener("DOMContentLoaded", function () {
 
         window.addEventListener("scroll", function(){
-            // event.preventDefault();
             const scrollY = window.scrollY;
             const masthead = document.getElementById('masthead');
 
@@ -11,13 +10,11 @@
                 masthead.classList.add('fixed-header');
                 $('.green-header-logo').css("display", "block");
                 $('.white-header-logo').css("display", "none");
-                $('.fa').css({"color": "#248a83", "text-shadow": "none"});
                 
             } else  {
                 masthead.classList.remove('fixed-header');
                 $('.green-header-logo').css("display", "none");
                 $('.white-header-logo').css("display", "block");
-                $('.fa').css({"color": "white", "text-shadow": "1px 1px 2px #323232"});
             };
         });
     });
